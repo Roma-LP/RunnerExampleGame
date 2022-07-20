69ff8d3f8603f309f96f7c6a270488ccbdaccaf9
Roma <rlepesh@mail.ru>
2022-07-20 18:25:16 +0300

    Add Player, mover, enemy, spawner, pool

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private int _health;
+
+    public void ApplyDamage(int damage)
+    {
+        _health -= damage;
+
+        if(damage <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        throw new NotImplementedException();
+    }
+}
